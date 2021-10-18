45245e25ba5f36a60ca6b2d6644850bfa7022efa
Saswat Das <68921527+dassaswat@users.noreply.github.com>
2021-10-18 22:24:08 +0530

    removed a redundant file

--- main.py
+++
@@ -1,4 +0,0 @@
-from downloader import VideoDownloader
-
-video_downloader = VideoDownloader(path = "/Users/saswatdas/Documents", log_to_file=False, convert_to_mp4= False)
-video_downloader.create_folders_and_download_data()
